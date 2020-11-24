d9c3b1c009b819e6c474f8bfecdf65220fedccfe
Peter Lin <plin0009@gmail.com>
2020-11-24 12:12:50 -0500

    Clickable and slightly shifted down logo

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,155 +1,230 @@
 /* eslint-disable react/jsx-no-duplicate-props */
-import React, { useEffect, Component } from 'react';
-import { NavDropdown, Navbar, Dropdown } from 'react-bootstrap';
-import { useMediaQuery } from 'react-responsive';
-import './header.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link } from 'react-router-dom';
-import logo from './img/logo.png';
-import otherLogo from './img/redYouthCouncil.png';
-import * as $ from 'jquery';
-
+import React, { useEffect, Component } from "react";
+import { NavDropdown, Navbar, Dropdown } from "react-bootstrap";
+import { useMediaQuery } from "react-responsive";
+import "./header.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from "react-router-dom";
+import logo from "./img/logo.png";
+import otherLogo from "./img/redYouthCouncil.png";
+import * as $ from "jquery";
 
 export default class Nav extends React.Component {
-    selectedLogo = logo;
-    constructor(props) {
-        super(props);
-        this.listener = null;
+  selectedLogo = logo;
+  constructor(props) {
+    super(props);
+    this.listener = null;
+  }
 
-    }
+  state = {
+    showHighLightInitiative: false,
+    showHighLightAbout: false,
+    showHighLightHome: false,
+    topOfPage: "top",
+  };
 
-    state = {
-        showHighLightInitiative: false,
-        showHighLightAbout: false,
-        showHighLightHome: false,
-        topOfPage: "top",
-    };
+  handleShowHighLightInitiativeToggle = () => {
+    this.setState({
+      showHighLightInitiative: !this.state.showHighLightInitiative,
+    });
+  };
 
+  handleShowHighLightAboutToggle = () => {
+    this.setState({ showHighLightAbout: !this.state.showHighLightAbout });
+  };
 
-    handleShowHighLightInitiativeToggle = () => {
-        this.setState({ showHighLightInitiative: !this.state.showHighLightInitiative });
-    }
+  handleShowHighLightHomeToggle = () => {
+    this.setState({ showHighLightHome: !this.state.showHighLightHome });
+  };
 
-    handleShowHighLightAboutToggle = () => {
-        this.setState({ showHighLightAbout: !this.state.showHighLightAbout });
-    }
+  componentDidMount() {
+    this.listener = document.addEventListener("scroll", (e) => {
+      var scrolled = document.scrollingElement.scrollTop;
+      if (scrolled > 15) {
+        if (this.state.status !== "bottom") {
+          this.setState({ topOfPage: "bottom" });
+          $("#Nav").addClass("bottom");
+        }
+      } else {
+        if (this.state.status !== "top") {
+          this.setState({ topOfPage: "top" });
+          $("#Nav").removeClass("bottom");
+        }
+      }
+    });
+  }
 
-    handleShowHighLightHomeToggle = () => {
-        this.setState({ showHighLightHome: !this.state.showHighLightHome });
-    }
+  componentDidUpdate() {
+    document.removeEventListener("scroll", this.listener);
+  }
 
-    componentDidMount() {
-        this.listener = document.addEventListener("scroll", e => {
-            var scrolled = document.scrollingElement.scrollTop;
-            if (scrolled > 15) {
-                if (this.state.status !== "bottom") {
-                    this.setState({ topOfPage: "bottom" });
-                    $("#Nav").addClass("bottom");
-                }
-            } else {
-                if (this.state.status !== "top") {
-                    this.setState({ topOfPage: "top" });
-                    $("#Nav").removeClass("bottom");
-                }
-            }
-        });
-    }
+  render() {
+    return (
+      <div
+        id="Nav"
+        style={{
+          backgroundColor:
+            this.state.topOfPage === "top" ? "transparent" : "white",
+          transition: "all 1.0s",
+          position: "fixed",
+        }}
+        className="NavBar"
+      >
+        <a href="/" className="NavBar-Logo">
+          <img id="logo" src={this.selectedLogo} alt="HfH Logo" />
+        </a>
 
-    componentDidUpdate() {
-        document.removeEventListener("scroll", this.listener);
-    }
+        <nav id="secondaryBar">
+          <div id="container-element">
+            <div className="btn-group">
+              <button
+                id="sandwichButton"
+                className="btn dropdown-toggle"
+                data-toggle="dropdown"
+              >
+                <img
+                  id="menubar"
+                  src="https://img.icons8.com/material-rounded/36/000000/menu.png"
+                  alt=""
+                />
+              </button>
+              <div className="dropdown-menu">
+                <Link to="/">
+                  <li
+                    onMouseEnter={this.handleShowHighLightHomeToggle}
+                    class="dropdown-item"
+                    onMouseLeave={this.handleShowHighLightHomeToggle}
+                    className={`item${
+                      this.state.showHighLightHome
+                        ? " entrance designStyle"
+                        : " exit designStyle"
+                    }`}
+                    style={{
+                      transition: "all 0.8s",
+                    }}
+                  >
+                    HOME
+                  </li>
+                </Link>
 
-    render() {
-        return (
+                <Link to="/about">
+                  <li
+                    onMouseEnter={this.handleShowHighLightAboutToggle}
+                    class="dropdown-item"
+                    onMouseLeave={this.handleShowHighLightAboutToggle}
+                    className={`item${
+                      this.state.showHighLightAbout
+                        ? " entrance designStyle"
+                        : " exit designStyle"
+                    }`}
+                    style={{
+                      transition: "all 0.8s",
+                    }}
+                  >
+                    ABOUT US
+                  </li>
+                </Link>
 
-            <div id="Nav"
-                style={{
-                    backgroundColor: this.state.topOfPage === "top" ? "transparent" : "white",
-                    transition: "all 1.0s",
-                    position: "fixed",
-                }}
-                className="NavBar">
-                <img id="logo" src={this.selectedLogo} alt="HfH Logo" />
-
-                <nav id="secondaryBar">
-                    <div id="container-element">
-                        <div className="btn-group">
-                            <button id="sandwichButton" className="btn dropdown-toggle"
-                                data-toggle="dropdown">
-                                <img id="menubar" src="https://img.icons8.com/material-rounded/36/000000/menu.png" alt="" />
-                            </button>
-                            <div className="dropdown-menu">
-                                <Link to="/">
-                                    <li onMouseEnter={this.handleShowHighLightHomeToggle} class="dropdown-item"
-                                        onMouseLeave={this.handleShowHighLightHomeToggle}
-                                        className={`item${this.state.showHighLightHome ? " entrance designStyle" : " exit designStyle"}`}
-                                        style={{
-                                            transition: "all 0.8s",
-                                        }}
-                                    >HOME</li>
-                                </Link>
-
-                                <Link to="/about">
-                                    <li onMouseEnter={this.handleShowHighLightAboutToggle} class="dropdown-item"
-                                        onMouseLeave={this.handleShowHighLightAboutToggle}
-                                        className={`item${this.state.showHighLightAbout ? " entrance designStyle" : " exit designStyle"}`}
-                                        style={{
-                                            transition: "all 0.8s",
-                                        }}
-                                    >ABOUT US</li>
-                                </Link>
-                                
-                                <Link to="/initiatives">
-                                    <li onMouseEnter={this.handleShowHighLightInitiativeToggle} class="dropdown-item"
-                                        onMouseLeave={this.handleShowHighLightInitiativeToggle}
-                                        className={`item${this.state.showHighLightInitiative ? " entrance designStyle" : " exit designStyle"}`}
-                                        style={{
-                                            transition: "all 0.8s",
-                                        }}
-                                    >INITIATIVES</li>
-                                </Link>
-                                
-                            </div>
-                        </div>
-                    </div>
-                </nav>
-
-                    <nav id="privaryNav" className="navbar navbar-light navbar-expand-lg bg-aqua sticky-top">
-                        <ul className="navbar-links primaryBar">
-
-                            <Link to="/initiatives">
-                                <li onMouseEnter={this.handleShowHighLightInitiativeToggle}
-                                    onMouseLeave={this.handleShowHighLightInitiativeToggle}
-                                    className={`${ this.state.topOfPage === "top" ? `item${this.state.showHighLightInitiative ? " enter " : " leave "}` : `element${this.state.showHighLightInitiative ? " noChange" : " changeFontColor"}`}`}
-                                    style={{
-                                        transition: "all 0.8s",
-                                    }}
-                                >INITIATIVES</li>
-                            </Link>
-
-                            <Link to="/about">
-                                <li onMouseEnter={this.handleShowHighLightAboutToggle}
-                                    onMouseLeave={this.handleShowHighLightAboutToggle}
-                                    className={`${ this.state.topOfPage === "top" ? `item${this.state.showHighLightAbout ? " enter " : " leave "}` : `element${this.state.showHighLightAbout ? " noChange" : " changeFontColor"}`}`}
-                                    style={{
-                                        transition: "all 0.8s",
-                                    }}
-                                >ABOUT US</li>
-                            </Link>
-
-                            <Link to="/">
-                                <li onMouseEnter={this.handleShowHighLightHomeToggle}
-                                    onMouseLeave={this.handleShowHighLightHomeToggle}
-                                    className={`${ this.state.topOfPage === "top" ? `item${this.state.showHighLightHome ? " enter " : " leave "}` : `element${this.state.showHighLightHome ? " noChange" : " changeFontColor"}`}`}
-                                    style={{
-                                        transition: "all 0.8s",
-                                    }}
-                                >HOME</li>
-                            </Link>
-                        </ul>
-                    </nav>
+                <Link to="/initiatives">
+                  <li
+                    onMouseEnter={this.handleShowHighLightInitiativeToggle}
+                    class="dropdown-item"
+                    onMouseLeave={this.handleShowHighLightInitiativeToggle}
+                    className={`item${
+                      this.state.showHighLightInitiative
+                        ? " entrance designStyle"
+                        : " exit designStyle"
+                    }`}
+                    style={{
+                      transition: "all 0.8s",
+                    }}
+                  >
+                    INITIATIVES
+                  </li>
+                </Link>
+              </div>
             </div>
-        );
-    }
-}
+          </div>
+        </nav>
 
+        <nav
+          id="privaryNav"
+          className="navbar navbar-light navbar-expand-lg bg-aqua sticky-top"
+        >
+          <ul className="navbar-links primaryBar">
+            <Link to="/initiatives">
+              <li
+                onMouseEnter={this.handleShowHighLightInitiativeToggle}
+                onMouseLeave={this.handleShowHighLightInitiativeToggle}
+                className={`${
+                  this.state.topOfPage === "top"
+                    ? `item${
+                        this.state.showHighLightInitiative
+                          ? " enter "
+                          : " leave "
+                      }`
+                    : `element${
+                        this.state.showHighLightInitiative
+                          ? " noChange"
+                          : " changeFontColor"
+                      }`
+                }`}
+                style={{
+                  transition: "all 0.8s",
+                }}
+              >
+                INITIATIVES
+              </li>
+            </Link>
+
+            <Link to="/about">
+              <li
+                onMouseEnter={this.handleShowHighLightAboutToggle}
+                onMouseLeave={this.handleShowHighLightAboutToggle}
+                className={`${
+                  this.state.topOfPage === "top"
+                    ? `item${
+                        this.state.showHighLightAbout ? " enter " : " leave "
+                      }`
+                    : `element${
+                        this.state.showHighLightAbout
+                          ? " noChange"
+                          : " changeFontColor"
+                      }`
+                }`}
+                style={{
+                  transition: "all 0.8s",
+                }}
+              >
+                ABOUT US
+              </li>
+            </Link>
+
+            <Link to="/">
+              <li
+                onMouseEnter={this.handleShowHighLightHomeToggle}
+                onMouseLeave={this.handleShowHighLightHomeToggle}
+                className={`${
+                  this.state.topOfPage === "top"
+                    ? `item${
+                        this.state.showHighLightHome ? " enter " : " leave "
+                      }`
+                    : `element${
+                        this.state.showHighLightHome
+                          ? " noChange"
+                          : " changeFontColor"
+                      }`
+                }`}
+                style={{
+                  transition: "all 0.8s",
+                }}
+              >
+                HOME
+              </li>
+            </Link>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
